54dde10260b10433d7f80dbcd3839797a4d6cb15
ananthapadmanabhan-o <ananthan51ah@gmail.com>
2024-06-20 15:12:45 +0530

    added streamlit

--- src/denoisingEncoder/pipeline/prediction.py
+++ src/denoisingEncoder/pipeline/prediction.py
@@ -1,32 +1,37 @@
-from denoisingEncoder.utils.common import load_pkl,read_yaml
-from denoisingEncoder.constants import PARAMS_FILE_PATH
+from denoisingEncoder.utils.common import load_pkl,read_yaml,create_directories
+from denoisingEncoder.constants import PARAMS_FILE_PATH,CONFIG_FILE_PATH
 from pathlib import Path
 from skimage.io import imread
+import matplotlib.pyplot as plt 
 from skimage.transform import resize
 import numpy as np
 from denoisingEncoder import logger
 
 
 
-
 class PredictionPipeline:
     def __init__(self,img_file):
         self.img_file = img_file
+        self.config = read_yaml(CONFIG_FILE_PATH)
         self.params = read_yaml(PARAMS_FILE_PATH)
-        print(self.params)
 
 
     def predict(self):
         img = (imread(self.img_file)/255).astype(np.float32)
         logger.info('image loaded successfully')
-        img = resize(img,(256,256,3))
+        img = resize(img,(self.params.IMAGE_WIDTH,self.params.IMAGE_HEIGHT,self.params.IMAGE_CHANNEL))
         logger.info('image resized successfully')
         img = np.array([img])
 
-        model = load_pkl(Path('artifacts/training/trained_model.pkl'))
+        model = load_pkl(Path(self.config.training.trained_model_path))
         result = model.predict(img)
         logger.info('Model predicted successfully')
         output_image = result[0,:]
+
+        create_directories([self.config.predict.root_dir])
+
+        with open(Path(self.config.predict.output_image_path),'wb') as file:
+            plt.imsave(file,output_image)
 
         return output_image
 
